a80c61cd3d8b8c08be80da47f66e70e92107c122
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-06-15 19:24:33 +0700

    Initial Commit

---
+++ MyMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMessenger
+//
+//  Created by Tan Ngo Dang on 15/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMessenger/MyMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMessengerApp.swift
+//  MyMessenger
+//
+//  Created by Tan Ngo Dang on 15/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
